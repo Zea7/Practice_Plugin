8575f54d6eb24ca4841d98da829271a7d49759ab
zea7 <zeaear714@gmail.com>
2020-12-19 22:44:44 +0900

    12/19

--- src/main/java/io/github/Zea7/plugin1/Plugin1.java
+++ src/main/java/io/github/Zea7/plugin1/Plugin1.java
@@ -9,7 +9,7 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin1 extends JavaPlugin {
-
+    public int set_Yield;
 
     @Override
     public void onEnable() {
@@ -34,12 +34,24 @@ public class Plugin1 extends JavaPlugin {
                 sender.sendMessage(ChatColor.YELLOW + "This Plugin is Made By Zea");
                 sender.sendMessage(ChatColor.YELLOW + "/z on : tntArrow를 활성화 합니다.");
                 sender.sendMessage(ChatColor.YELLOW + "/z off : tntArrow를 비활성화 합니다.");
+                sender.sendMessage(ChatColor.YELLOW + "/tntArrow : tntArrow의 기본 값을 바꿉니다.");
             } else if (args[0].equalsIgnoreCase("on")) {
                 getServer().getPluginManager().registerEvents(e, this);
                 sender.sendMessage(ChatColor.YELLOW+"tntArrow가 활성화 되었습니다.");
-            }else if(args[0].equalsIgnoreCase("off")){
+            }else if(args[0].equalsIgnoreCase("off")) {
                 ProjectileHitEvent.getHandlerList().unregister(this);
-                sender.sendMessage(ChatColor.YELLOW+"tntArrow가 비활성화 되었습니다.");
+                sender.sendMessage(ChatColor.YELLOW + "tntArrow가 비활성화 되었습니다.");
+            }else if(args[0].equalsIgnoreCase("tntArrow")){
+                if(args[1].equalsIgnoreCase("set")){
+                    set_Yield = Integer.parseInt(args[2]);
+                    getServer().getPluginManager().registerEvents(e,this);
+                }
+                else if(args[1].equalsIgnoreCase("help")){
+                     sender.sendMessage(ChatColor.YELLOW + "/tntArrow set [숫자] : 폭발 범위를 [숫자]만큼으로 정합니다.");
+                }
+                else{
+                    sender.sendMessage(ChatColor.RED+"올바르지 않은 입력입니다.");
+                }
             }else {
                 sender.sendMessage(ChatColor.RED + "명령어가 정확하지 않습니다.");
             }
